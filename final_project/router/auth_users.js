952cfdcceaec01395f640903d2e56021dfe8e090
Niladri Saha <sahaniladri27y@gmail.com>
2026-09-12 16:59:37 -0400

    update code for customer login and define the add or update book review function

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -25,26 +25,57 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  const username = req.body.username;
-  const password = req.body.password;
-  if(!username || !password){
-    return res.status(404).json({message: "Error logging in!"});
+  try{
+    const username = req.body.username;
+    const password = req.body.password;
+    if(!username || !password){
+      return res.status(404).json({message: "Error logging in!"});
+    }
+  
+    if(!authenticatedUser(username, password)){
+      return res.status(401).json({message: "Invalid Login data, check username and password!"});
+    }
+    const accessToken = jwt.sign({data: password}, "access", {expiresIn: 60 * 60});
+    if(req.session){
+      req.session.authorization = {
+        accessToken, username
+      };
+      return res.status(200).json({message: "User successfully logged in!", token: accessToken});
+    }
+  }catch(err){
+    return res.status(500).json({ message: "Internal server error." });
   }
-
-  if(!authenticatedUser(username, password)){
-    return res.status(208).json({message: "Invalid Login data, check username and password!"});
-  }
-  const accessToken = jwt.sign({data: password}, "access", {expiresIn: 60 * 60});
-  req.session.authorization = {
-    accessToken, username
-  };
-  return res.status(200).json({message: "User successfully logged in!"});
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  try{
+    const isbn = req.params.isbn;
+    if(!isbn){
+      return res.status(400).json({message: "ISBN not found!"});
+    }
+  
+    const bookFound = books[isbn];
+    if(!bookFound){
+      return res.status(404).json({message: "No such book having this ISBN found!"});
+    }
+  
+    const loggedInUser = req.session?.authorization?.username;
+    if(!loggedInUser){
+      return res.status(401).json({message: "User not authenticated!"});
+    }
+  
+    const {review} = req.body;
+    if(!review){
+      return res.status(400).json({message: "Review text is required!"});
+    }
+    
+    bookFound.reviews = bookFound.reviews || {};
+    bookFound.reviews[loggedInUser] = review;
+    return res.status(200).json({ message: "Review added/updated.", reviews: bookFound.reviews })
+  }catch(err){
+    return res.status(500).json({message: "Internal server error!"});
+  }
 });
 
 module.exports.authenticated = regd_users;
